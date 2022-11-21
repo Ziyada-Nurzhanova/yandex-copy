61bd35df19b1a62b6843f90b2ccfbc0720acca40
Alice <aliceerror380@gmail.com>
2022-11-22 01:58:24 +0600

    added new cmd

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 // Обьявление переменной и присвоения ей значения ввода пользователем
 let mood = prompt("How are you?");
 //Вывод данных в консоль
-console.log("Your mood is " + mood);
+document.writeln("Your mood is " + mood);
 // Вывод данных в всплывающем окне
 alert("You are " + mood);
 // Вывод сообщения в документе
@@ -16,3 +16,17 @@ document.write(mood + first + second);
 first += 2;
 // Выполняет математические операции, также часто можно писать html-элементы
 document.write("<br>" + Math.PI);
+// Массивы, в массивах отчет элементов начинается с 0
+let colors = ["Red", "Blue", "Green"];
+document.writeln(colors[0]);
+// Команда new позволяет добавлять в массив новые элементы
+let colorsf = new Array("Red", "Blue", "Green");
+colorsf[3] = "Grey";
+document.writeln(colorsf[3]);
+// В массивы можно записывать абсолютно оюбые значения: строки, булевые значения и числа с плавающей точкой
+// Команда push добавляет новый элемент в конец массива и является более удобным
+colorsf.push("Yellow");
+// Циклы
+for (let i = 10; i > 10; i++) {
+  console.log(i);
+}
